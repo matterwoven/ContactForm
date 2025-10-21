f05cd2b66cdc41127e21cb9c7c826d4cf160fc9e
Matthew Vargas <matthewvargas2013@gmail.com>
2025-10-20 18:54:37 -0700

    App finished

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const path = require('path');
 
+// Import the express module
+import express from 'express';
+
+// Create an instance of an Express application
 const app = express();
-const port = 3010;
 
-// Serve static files from the current directory
-app.use(express.static(__dirname));
+// Define the port number where our server will listen
+const PORT = 3012;
 
+app.use(express.static('public'));
+
+
+// Define a default "route" ('/')
+// req: contains information about the incoming request
+// res: allows us to send back a response to the client
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+
+
+    res.sendFile(`${import.meta.dirname}/views/index.html`);
+
+
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
+// Start the server and listen on the specified port 
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+}); 
