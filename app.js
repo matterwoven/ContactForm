41949322f5ff5b81ca8e4d932494e3f8681bfa60
Matthew Vargas <matthewvargas2013@gmail.com>
2025-11-02 19:18:01 -0800

    change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import express from 'express';
 const app = express();
 
 // Define the port number where our server will listen
-const PORT = 3028;
+const PORT = 3068;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
