cdf8ad991039bbb15f382e5156f53db13ccd3c5e
Matthew Vargas <matthewvargas2013@gmail.com>
2025-11-03 08:23:17 -0800

    IT WORKS???

--- app.js
+++ app.js
@@ -17,13 +17,8 @@ const guestbookArray = []
 // req: contains information about the incoming request
 // res: allows us to send back a response to the client
 app.get('/', (req, res) => {
-
-
     res.sendFile(`${import.meta.dirname}/views/index.html`);
-
-
 });
-
 
 app.get("/confirmation", (req, res) => {
     console.log(guestbookArray);
